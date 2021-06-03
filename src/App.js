bbd9af1abba31ddc31eed049b77d3cce31a8244f
stojke90 <stojke90@gmail.com>
2021-06-03 23:27:11 +0200

    problem with get consuption for 3 first country

--- src/App.js
+++ src/App.js
@@ -10,38 +10,59 @@ const baseUrl = `http://api.eia.gov`;
 const apiKey = 'api_key=c10de4f134f66672b5c80ff6c0eda8c4';
 
 const App = () => {
+
+
+  // state for total world production for next year
   const [worldProduction, setWorldProduction] = useState([]);
-  // total world production for next year
-  const TotalWorldPetroleumProductionAnnual = () => {
-    axios.get(`${baseUrl}/series/?${apiKey}&series_id=STEO.PAPR_WORLD.A`)
-    .then(res => (res.status === 200 || res.status === 201) && setWorldProduction([res.data.series[0]]));
-  }
 
-  useEffect(() => {
-    TotalWorldPetroleumProductionAnnual();
-  },[]);
-
-  // total world production for 2018 year
+  // state for total world production for 2018 year
   const [worldConsumption, setWorldConsumption] = useState([]);
-  const WorldOilConsumption  = () => {
-    axios.get(`${baseUrl}/series/?${apiKey}&series_id=INTL.4415-2-WORL-QBTU.A`)
-    .then(res => (res.status === 200 || res.status === 201) && setWorldConsumption(res.data.series));
-  }
-  useEffect(() => {
-    WorldOilConsumption();
-  },[]);
+
+  // serialID of countrys,consuption oil
+  const [serialIdConsuption , setSerialIdConsuption] = useState([]);
+
+  // state for countrys consuption oil and number of consuption oil by year,anual
+  const [allCountrysConsuption, setAllCountrysConsuption] = useState([]);
+  const [first100Filter, setFirst100Filter] = useState([]);
+  const [first200Filter, setFirst200Filter] = useState([]);
+  const [first300Filter, setFirst300Filter] = useState([]);
+
+  // state for serialID of countrys,product oil
+  const [serialIdProducing , setSerialIdProducing] = useState([]);
+
+  // countrys product oil and number of product oil
+  const [allCountrysProducing, setAllCountrysProducing] = useState([]);
+
+  const [lastYear ,setLastYear] = useState('');
 
   // set last year
   const year = () => {
     const date = new Date();
     const lastYear = (date.getFullYear() - 1).toString();
-    return lastYear;
+    setLastYear(lastYear);
+    console.log("year1")
+  };
+
+
+  // total world production for next year
+  const totalWorldPetroleumProductionAnnual = () => {
+    axios.get(`${baseUrl}/series/?${apiKey}&series_id=STEO.PAPR_WORLD.A`)
+    .then(res => (res.status === 200 || res.status === 201) && setWorldProduction([res.data.series[0]]));
+    console.log("TotalWorldPetroleumProductionAnnual2")
   }
 
+
+  // total world production for 2018 year 
+  const worldOilConsumption  = () => {
+    axios.get(`${baseUrl}/series/?${apiKey}&series_id=INTL.4415-2-WORL-QBTU.A`)
+    .then(res => (res.status === 200 || res.status === 201) && setWorldConsumption(res.data.series));
+    console.log("WorldOilConsumption3")
+  }
+
+
+
   // serialID of countrys,consuption oil
-  const [serialIdConsuption , setSerialIdConsuption] = useState([]);
-  // serialID of countrys,consuption oil
-  const OilConsuptionByCountry = () => {
+  const oilConsuptionByCountrySerialId = () => {
     axios.get(`${baseUrl}/category/?${apiKey}&category_id=2134436`)
     .then(res => 
       (res.status === 200 || res.status === 201) &&
@@ -49,54 +70,54 @@ const App = () => {
         .filter(data => data.units === "thousand barrels per day")
         .map(serialId => serialId.series_id)))
     .catch(error => alert(error));
-  }
+    console.log("OilConsuptionByCountrySerialId4")
+  };
 
-  useEffect(() => {
-    OilConsuptionByCountry();
-  },[]);
+  // const first100 = [];
+  // const first200 = [];
+  // const first300 = [];
 
-      const first100 = [];
-      const first200 = [];
-      const first300 = [];
+  //   if(serialIdConsuption.length > 201) {
+  //     for(let i = 0; i <= 99; i++){
+  //       first100[first100.length] = serialIdConsuption[i];
+  //     }
+  //     for(let i = 100; i <= 199; i++){
+  //       first200[first200.length] = serialIdConsuption[i];
+  //     }
+  //     for(let i = 200; i <= serialIdConsuption.length - 1; i++){
+  //       first300[first300.length] = serialIdConsuption[i];
+  //     }
+  //   };
 
-  if(serialIdConsuption.length > 1) {
-      for(let i = 0; i <= 99; i++){
-        first100[first100.length] = serialIdConsuption[i];
-      }
-      for(let i = 100; i <= 199; i++){
-        first200[first200.length] = serialIdConsuption[i];
-      }
-      for(let i = 200; i <= serialIdConsuption.length - 1; i++){
-        first300[first300.length] = serialIdConsuption[i];
-      }
-  }
-
-  // countrys consuption oil and number of consuption oil by year,anual
-  const [allCountrysConsuption, setAllCountrysConsuption] = useState([]);
-  const [first100Filter, setFirst100Filter] = useState([]);
-  const [first200Filter, setFirst200Filter] = useState([]);
-  const [first300Filter, setFirst300Filter] = useState([]);
   // countrys consuption oil and number of consuption oil by year,anual
   const countrysWithMostConsumption = () => {
-    axios.get(`${baseUrl}/series/?${apiKey}&series_id=${first100.join(";")}`)
-    .then(res =>
-     (res.status === 200 || res.status === 201) && 
-     setFirst100Filter(res.data.series.filter(data => data.data[0][0] === year())))
-    .catch(error => alert(error));
+    const first100 = serialIdConsuption.slice(0,100).map(data => data + ";").join("");
+    const first200 = serialIdConsuption.slice(100,200).map(data => data + ";").join("");
+    const first300 = serialIdConsuption.slice(200,300).map(data => data + ";").join("");
+    console.log(first100);
+    console.log(first200);
+    console.log(first300);
 
-    axios.get(`${baseUrl}/series/?${apiKey}&series_id=${first200.join(";")}`)
-    .then(res =>
-     (res.status === 200 || res.status === 201) && 
-     setFirst200Filter(res.data.series.filter(data => data.data[0][0] === year())))
-    .catch(error => alert(error));
+    axios.all([
+      axios.get(`${baseUrl}/series/?${apiKey}&series_id=${first100}`), 
+      axios.get(`${baseUrl}/series/?${apiKey}&series_id=${first200}`),
+      axios.get(`${baseUrl}/series/?${apiKey}&series_id=${first300}`)
+    ])
+    .then(axios.spread((obj1, obj2, obj3) => {
 
-    axios.get(`${baseUrl}/series/?${apiKey}&series_id=${first300.join(";")}`)
-    .then(res =>
-     (res.status === 200 || res.status === 201) && 
-     setFirst300Filter(res.data.series.filter(data => data.data[0][0] === year())))
-    .catch(error => alert(error));
-  }
-
+      (obj1.statusText === 200 || obj1.status === 201) && 
+        console.log(obj1.data.series.filter(data => data.data[0][0] === lastYear));
+      console.log(obj1);
+      (obj2.status === 200 || obj2.status === 201) && 
+        console.log(obj2.data.series.filter(data => data.data[0][0] === lastYear));
+      // console.log(obj2.data.series);
+      (obj3.status === 200 || obj3.status === 201) && 
+        console.log(obj3.data.series.filter(data => data.data[0][0] === lastYear));
+      // console.log(obj3.data.series);
+    }))
+    // .catch(errors => alert(errors));
+    console.log("countrysWithMostConsumption5");
+  };
 
   useEffect(() => {
     setAllCountrysConsuption(
@@ -104,14 +125,20 @@ const App = () => {
       .concat(first200Filter)
       .concat(first300Filter)
       .sort((a,b) => { 
-        return a.data[0][1] - b.data[0][1]}).reverse()
+        return a.data[0][1] - b.data[0][1]
+      })
+      .reverse()
+      .slice(0,3)
       );
-
+    console.log("useEffect,setAllCountrysConsuption6")
   },[first100Filter,first200Filter,first300Filter]);
 
+  // console.log("prvih 100 serialID filtriranih " + first100Filter)
+  // console.log("prvih 200 serialID filtriranih " + first200Filter)
+  // console.log("prvih 300 serialID filtriranih " + first300Filter)
+  // console.log(allCountrysConsuption)
 
-  // serialID of countrys,product oil
-  const [serialIdProducing , setSerialIdProducing] = useState([]);
+
   // serialID of countrys,product oil
   const oilProducingCountriesSerialId = () => {
     axios
@@ -123,9 +150,9 @@ const App = () => {
         .map(data => data.series_id).join(";"))
       )
     .catch(error => alert(error));
-  }
-  // countrys product oil and number of product oil
-  const [allCountrysProducing, setAllCountrysProducing] = useState([]);
+    console.log("oilProducingCountriesSerialId7")
+  };
+
   // countrys product oil and number of product oil
   const oilProducingCountries = () => {
     axios.get(`${baseUrl}/series/?${apiKey}&series_id=${serialIdProducing}`)
@@ -133,19 +160,24 @@ const App = () => {
       (res.status === 200 || res.status === 201) && 
       setAllCountrysProducing(res.data.series))
     .catch(error => alert(error));
-  }
+    console.log("oilProducingCountries8")
+  };
 
   useEffect(() => {
-    oilProducingCountriesSerialId()
+    year();
+    totalWorldPetroleumProductionAnnual();
+    worldOilConsumption();
+    oilConsuptionByCountrySerialId();
+    oilProducingCountriesSerialId();
   },[]);
 
   useEffect(() => {
-    oilProducingCountries()
+    oilProducingCountries();
   },[serialIdProducing]);
 
-  useEffect(() => {
-    countrysWithMostConsumption();
-  },[serialIdConsuption])
+  // useEffect(() => {
+  //     countrysWithMostConsumption(); 
+  // },[serialIdProducing]);
 
   return (
     <div className="App">
@@ -171,7 +203,7 @@ const App = () => {
       <div>
         <h1>Oil Consuption Countries Last Year</h1>
         {allCountrysConsuption ? 
-          allCountrysConsuption.slice(0,3).map(data => <p key={uuid()}>
+          allCountrysConsuption.map(data => <p key={uuid()}>
             {data.name.split(",")[data.name.split(",").length - 2] + " => "}
             {"last year,anual = " + data.data[0][1]}
             </p>
